f48e98ae70cc292649360c6ee24ce695bafcbe43
Matthew <matthew.qian2@gmail.com>
2023-11-24 09:11:33 +1100

    create item card array

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,14 @@ import ItemCard from "../ItemCard";
 import Navbar from "../NavBar";
 
 export default function Home() {
+    const items = [1, 2, 3, 4, 5, 6, 7, 8];
+
     return <div>
         <Navbar></Navbar>
-        <ItemCard></ItemCard>
+        <div className="itemCardArray">
+            {items.map(() => (
+                <ItemCard/>
+            ))}
+        </div>
         </div>
   }
