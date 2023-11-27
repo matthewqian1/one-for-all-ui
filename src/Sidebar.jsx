bb54fda7b26beb8345ef87e0a2d131a08fea9b0d
Matthew <matthew.qian2@gmail.com>
2023-11-28 09:04:49 +1100

    add sample side bar

---
+++ src/Sidebar.jsx
@@ -0,0 +1,28 @@
+
+
+function Sidebar() {
+
+    const closeNav = () => {
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("sidebarTrigger").style.marginLeft= "0";
+    }
+    const openNav = () => {
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("sidebarTrigger").style.marginLeft = "250px";
+    }
+
+  return <div><div id="mySidebar" class="sidebar">
+  <a className="closebtn" onClick={closeNav}>X</a>
+  <a >About</a>
+  <a >Services</a>
+  <a >Clients</a>
+  <a >Contact</a>
+</div>
+<div id="sidebarTrigger">
+  <button class="openbtn" onClick={openNav}>☰ Open Sidebar</button>  
+</div>
+</div>
+}
+
+export default Sidebar;
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 
 import ItemCard from "../ItemCard";
 import Navbar from "../NavBar";
+import Sidebar from "../Sidebar";
 
 export default function Home() {
     const items = [1, 2, 3, 4, 5, 6, 7, 8];
 
     return <div>
+        <Sidebar></Sidebar>
         <Navbar></Navbar>
         <div className="itemCardArray">
             {items.map(() => (
