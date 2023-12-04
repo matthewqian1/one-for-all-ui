58a5a3e84769ee16c050a47bebb3cbf12404b4ed
Matthew <matthew.qian2@gmail.com>
2023-12-05 09:12:21 +1100

    add listing routing

--- src/ItemCard.jsx
+++ src/ItemCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
 class ItemCard extends React.Component {
     constructor(props) {
@@ -6,13 +7,25 @@ class ItemCard extends React.Component {
         this.state = {
             base64Data: props.data.image,
             description: props.data.description,
-            name: props.data.name
+            name: props.data.name,
+            id: props.data.id,
+            clicked: false
           };
+          this.handleClick = this.handleClick.bind(this);
     }
 
+    handleClick(e) {
+        e.preventDefault();
+        console.log("hello");
+        this.setState({clicked: true});
+      }
+
     render() {
-        const { base64Data, description, name } = this.state;
-        return <div className="itemCard">
+        const { base64Data, description, name, id, clicked } = this.state;
+        return <div className="itemCard" onClick={this.handleClick}>
+            {clicked && (
+          <Navigate to="/addProduct" replace={true} />
+        )}
             <h1>{name}</h1>
             <img src={`data:image;base64,${base64Data}`} />
             <p>{description}</p>
---
+++ src/pages/Listing.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
+export default function Listing() {
+    
+    const navigate = useNavigate();
+
+    return <div>
+        Hello
+
+    </div>
+    
+  }
