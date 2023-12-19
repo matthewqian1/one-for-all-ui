fea0c201eff0a6112ab07b108dbe773df8400238
Matthew <matthew.qian2@gmail.com>
2023-12-19 16:32:14 +1100

    style listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -15,7 +15,7 @@ export default function Listing() {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState(0.0);
-    let sizes = ['S', 'M', 'L'];
+    let sizes = ['S', 'M', 'L', 'XL', 'XXL'];
 
     const addToCart = (e) => {
         e.preventDefault();
@@ -39,27 +39,49 @@ export default function Listing() {
     }, []);
 
     const [size, setSize] = useState('S');
-    return <div>
-        <Sidebar></Sidebar>
-        <Navbar cart={myCart}></Navbar>
-        <div className='listing'>
-        <div style={{padding: "5%"}}>
-        <img src={`data:image;base64,${image}`} />
-        </div>
-        
-        <selection>
-            <h1>{name}</h1>
-            <h2>{description}</h2>
-            <h3>${price}</h3>
-            <p>size : {size}</p>
-            {sizes.map((size) => {
-                return <button className='button sizeButton' value={size} onClick={(e) => setSize(e.target.value)}>{size}</button>
-            })}
-            <br></br>
-            <button className='button addToCartButton' onClick={addToCart}>Add To Cart</button>
-        </selection>
 
-    </div>
+
+    return <div>
+            <Sidebar></Sidebar>
+            <Navbar cart={myCart}></Navbar>
+        <div className='listing'>
+        <div>
+        <img src={`data:image;base64,${image}`} />
+        <hr></hr>
+        <content>
+            <h1>Description</h1>
+            <p>{description}</p>
+        </content>
+        <hr></hr>
+        <content>
+            <h1>Shipping + Returns</h1>
+            <li>Free shipping within Australia on all orders of $100 or more</li>
+            <li>Simple and easy returns.</li>
+            <li>We do not offer exchanges, only refunds</li>
+            <li>Visit our customer service page for more information or contact our customer service team for further questions.</li>
+        </content>
+            </div>
+                <selection>
+                    <h1>{name}</h1>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <h3>${price}</h3>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <p>size : {size}</p>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    {sizes.map((size) => {
+                        return <button className='button sizeButton' value={size} onClick={(e) => setSize(e.target.value)}>{size}</button>
+                    })}
+                    <br></br>
+                    <button className='button addToCartButton' onClick={addToCart}>Add To Cart</button>
+                </selection>
+
+            </div>
     </div>
     
   }
