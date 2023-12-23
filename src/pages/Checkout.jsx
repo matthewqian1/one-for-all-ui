2e21f907824c4fbd9dcb5972dae64d2aba3588bd
Matthew <matthew.qian2@gmail.com>
2023-12-23 19:09:00 +1100

    add colour filter and finish formatting checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -31,34 +31,45 @@ export default function Checkout() {
       )
       
   });
+
+  const totalCost = cart.reduce((n, {price}) => n + parseFloat(price), 0);
+
     return <div>
         <Sidebar></Sidebar>
         <Navbar></Navbar>
         <div className='orderPage'>
-          <div className='orderForm'>
+          <form className='orderForm'>
             <h1>1. Your Email Address</h1>
             <label>Email Address *</label><br/>
-            <input type="text"/><br/>
+            <input type="text" required='true' value={'random@gmail.com'}/><br/>
             <hr></hr>
 
             <h1>2. Delivery Details</h1>
             <label>First Name *</label><br/>
-            <input type="text"/><br/>
+            <input type="text" required='true' value={'John'}/><br/>
             <label>Last Name *</label><br/>
-            <input type="text"/><br/>
+            <input type="text" required='true' value={'Doe'}/><br/>
             <label>Country *</label><br/>
-            <input type="text"/><br/>
+            <input type="text" required='true' value={'Australia'}/><br/>
 
             <label>Delivery Address *</label><br/>
-            <input type="text"/><br/>
+            <input type="text" required='true' value={'123 Kangaroo Rd'}/><br/>
             <label>Phone Number *</label><br/>
-            <input type="text"/><br/>
+            <input type="text" required='true' value={'+61 123 123 123'}/><br/>
             <hr></hr>
 
             <h1>1. Payment Details</h1>
-            <label>Email Address *</label><br/>
-            <input type="text"/><br/>
-          </div>
+            <label>Card Holder Name *</label><br/>
+            <input type="text" required='true' value={'John Doe'}/><br/>
+            <label>Card Number *</label><br/>
+            <input type="text" required='true' value={'1234 1234 1234 1234'}/><br/>
+            <label>Expiry Date *</label><br/>
+            <input type="month" style={{width: '150px'}} required='true' value="2028-07"/><br/>
+            <label>Security Code *</label><br/>
+            <input type="text" style={{width: '80px'}} required='true' value={123}/><br/>
+            <hr></hr>
+            <input type="submit" value="Place Order"/>
+          </form>
           <div className='orderSummary'>
             {cart.map((item, idx) => {
               return <div className="cartItem">
@@ -67,12 +78,22 @@ export default function Checkout() {
                           {item.name}
                         </div>
                         <div className="cartItemPrice">
-                          {item.price}
+                          ${item.price}
                         </div>
                       </div>
                       }
                       )
             }
+            <div className="cartTotalPrice">
+              <header>Subtotal:</header>
+              <value>${totalCost}</value>
+              <br/>
+              <header>Total Discount:</header>
+              <value>- ${0.2 * totalCost}</value>
+              <br/>
+              <header>Order Total:</header>
+              <value>${0.8 * totalCost}</value>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,8 @@ export default function Home() {
     const [priceFilter, setPriceFilter] = useState(0);
     const [allCategories, setAllCategories] = useState([]);
     const [categoryFilter, setCategoryFilter] = useState(new Set());
+    const [allColours, setAllColours] = useState([]);
+    const [colourFilter, setColourFilter] = useState(new Set());
 
     useEffect(() => {
         fetch(`${properties.BASE_URL}/product/all` , {
@@ -41,6 +43,21 @@ export default function Home() {
               }
               setAllCategories(list);
           })
+
+        fetch(`${properties.BASE_URL}/product/getColours` , {
+        method: 'GET',
+        headers: { "Content-Type": "application/json"}
+        })
+        .then(res => res.json()) 
+        .then(json => {
+            let list = [];
+            for(let i = 0; i < json.length; i++) {
+                let obj = json[i];
+            
+                list.push(obj);
+            }
+            setAllColours(list);
+        })
       }
     , []);
 
@@ -53,6 +70,14 @@ export default function Home() {
             setCategoryFilter(new Set([...categoryFilter, e.target.value]));
         } else {
             setCategoryFilter(new Set([...categoryFilter].filter(x => x !== e.target.value)))
+        }
+    }
+
+    const editColourFilter = (e) => {
+        if (e.target.checked) {
+            setColourFilter(new Set([...colourFilter, e.target.value]));
+        } else {
+            setColourFilter(new Set([...colourFilter].filter(x => x !== e.target.value)))
         }
     }
 
@@ -91,11 +116,26 @@ export default function Home() {
                         }
                     </form>
                 </div>
+                <div className="filter">
+                    <h1>Colour</h1>
+                    <form onChange={editColourFilter}>
+                        {allColours.map
+                            ((colour) => {
+                                return <>
+                                <input type="checkbox" value={colour}/>
+                                <label> {colour}</label><br/>
+                                </>
+                                }
+                            )
+                        }
+                    </form>
+                </div>
             </div>
             <div className="itemCardArray">
                 {products
                 .filter(product => parseInt(priceFilter) === 0 || product.price <= parseInt(priceFilter))
                 .filter(product => categoryFilter.size === 0 || categoryFilter.has(product.category))
+                .filter(product => colourFilter.size === 0 || colourFilter.has(product.colour))
                 .map((product) => 
                     (
                     <ItemCard key={product.id} data={{
