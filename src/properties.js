e2ead4c6960b74917258affc900f7c39681b93c2
Matthew <matthew.qian2@gmail.com>
2024-01-03 19:22:24 +1100

    adjust for prod env

--- src/properties.js
+++ src/properties.js
@@ -1,4 +1,12 @@
-export const properties = {
+const dev = {
     PRODUCT_URL: 'http://localhost:8080',
     AI_URL: 'http://localhost:8082'
 };
+
+const prod = {
+    PRODUCT_URL: 'https://one-for-all-product-service-7840cb2fb213.herokuapp.com',
+    AI_URL: 'https://one-for-all-ai-service-f9084e4163ee.herokuapp.com'
+};
+
+
+export const properties = process.env.NODE_ENV === 'production' ? prod : dev;
